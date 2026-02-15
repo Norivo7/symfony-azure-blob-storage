80ffcb42b134a70579ce3b2c11c5e66f03a4fd1a
Norivo7 <szymik.kamil97@gmail.com>
2026-02-15 14:41:37 +0100

    build: initial docker config, symfony skeleton

---
+++ app/src/Controller/TestController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class TestController extends AbstractController
+{
+    #[Route('/test', name: 'app_test')]
+    public function index(): JsonResponse
+    {
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/TestController.php',
+        ]);
+    }
+
+    #[Route('/', name: 'home')]
+    public function home(): Response
+    {
+        return $this->render('base.html.twig');
+    }
+}
